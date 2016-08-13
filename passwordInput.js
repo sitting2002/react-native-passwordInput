a5367c4ba46467776f020dff861c1a1bd042dfb7
消失的旅人 <869412057@qq.com>
2016-08-13 13:18:56 +0800

    使用ES6语法，添加onEnd事件，autoFocus效果优化

--- passwordInput.js
+++ passwordInput.js
@@ -2,82 +2,126 @@
  * Created by chenchunyong on 12/2/15.
  */
 
-import React,{StyleSheet,View,TextInput,Text,TouchableHighlight } from 'react-native';
+import React, {
+  Component,
+  PropTypes,
+} from 'react';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableHighlight,
+  InteractionManager,
+} from 'react-native';
 
-var styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        flexDirection: 'row',
-        borderWidth: 1,
-        borderColor: '#ccc',
-        backgroundColor: '#fff'
-    },
-    inputItem: {
-        height: 45,
-        width: 45,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    inputItemBorderLeftWidth: {
-        borderLeftWidth: 1,
-        borderColor: '#ccc',
-    },
-    iconStyle: {
-        width: 16,
-        height: 16,
-        backgroundColor: '#222',
-        borderRadius: 8,
-    },
-});
+export default class Password extends Component {
+  static propTypes = {
+    style: View.propTypes.style,
+    inputItemStyle: View.propTypes.style,
+    iconStyle: View.propTypes.style,
+    maxLength: TextInput.propTypes.maxLength.isRequired,
+    onChange: PropTypes.func,
+    onEnd: PropTypes.func,
+    autoFocus: PropTypes.bool,
+  };
 
-export default React.createClass({
-    getInitialState(){
-        return {
-            text: ''
-        };
-    },
-    _onPress(){
-        this.refs.textInput.focus();
-    },
+  static defaultProps = {
+    autoFocus: true,
+    onChange: () => {},
+    onEnd: () => {},
+  };
 
-    render(){
-        return(
-            <TouchableHighlight onPress={this._onPress} activeOpacity={1} underlayColor='transparent'>
-                <View style={[styles.container,this.props.style]} >
-                    <TextInput ref='textInput'
-                        maxLength={this.props.maxLength}
-                        autoFocus={true}
-                        keyboardType="numeric"
-                        onChangeText={(text)=>{this.setState({text});this.props.onChange(text)}}
-                        />
-                    {
-                        this._getInputItem()
-                    }
-                </View>
-            </TouchableHighlight>
-        )
+  state = {
+    text: ''
+  };
 
-    },
-    _getInputItem(){
-        let inputItem = [];
-        let {text}=this.state;
-        for (let i = 0; i < parseInt(this.props.maxLength); i++) {
-            if (i == 0) {
-                inputItem.push(
-                    <View key={i} style={[styles.inputItem,this.props.inputItemStyle]}>
-                        {i < text.length ? <View style={[styles.iconStyle,this.props.iconStyle]}></View> : null}
-                    </View>)
+  componentDidMount() {
+    if (this.props.autoFocus) {
+      InteractionManager.runAfterInteractions(() => {
+        this._onPress();
+      });
+    }
+  }
+
+  render(){
+    return(
+      <TouchableHighlight
+        onPress={this._onPress.bind(this)}
+        activeOpacity={1}
+        underlayColor='transparent'>
+        <View style={[styles.container,this.props.style]} >
+          <TextInput
+            ref='textInput'
+            maxLength={this.props.maxLength}
+            autoFocus={false}
+            keyboardType="number-pad"
+            onChangeText={
+              (text) => {
+                this.setState({text});
+                this.props.onChange(text);
+                if (text.length === this.props.maxLength) {
+                  this.props.onEnd(text);
+                }
+              }
             }
-            else {
-                inputItem.push(
-                    <View key={i} style={[styles.inputItem,styles.inputItemBorderLeftWidth,this.props.inputItemStyle]}>
-                        {i < text.length ?
-                            <View style={[styles.iconStyle,this.props.iconStyle]}>
-                            </View> : null}
-                    </View>)
-            }
-            ;
-        }
-        return inputItem;
-    },
+          />
+          {
+            this._getInputItem()
+          }
+        </View>
+      </TouchableHighlight>
+    )
+
+  }
+  _getInputItem(){
+    let inputItem = [];
+    let {text}=this.state;
+    for (let i = 0; i < parseInt(this.props.maxLength); i++) {
+      if (i == 0) {
+        inputItem.push(
+          <View key={i} style={[styles.inputItem,this.props.inputItemStyle]}>
+            {i < text.length ? <View style={[styles.iconStyle,this.props.iconStyle]} /> : null}
+          </View>)
+      }
+      else {
+        inputItem.push(
+          <View key={i} style={[styles.inputItem,styles.inputItemBorderLeftWidth,this.props.inputItemStyle]}>
+            {i < text.length ?
+              <View style={[styles.iconStyle,this.props.iconStyle]}>
+              </View> : null}
+          </View>)
+      }
+    }
+    return inputItem;
+  }
+
+  _onPress(){
+    this.refs.textInput.focus();
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    backgroundColor: '#fff'
+  },
+  inputItem: {
+    height: 45,
+    width: 45,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputItemBorderLeftWidth: {
+    borderLeftWidth: 1,
+    borderColor: '#ccc',
+  },
+  iconStyle: {
+    width: 16,
+    height: 16,
+    backgroundColor: '#222',
+    borderRadius: 8,
+  },
 });
